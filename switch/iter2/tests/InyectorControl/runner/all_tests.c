9ae6dabed2f9c4a64e1ae59595be0e7f45701a93
lf <francucci@itdelsat.com.ar>
2017-06-23 15:37:21 -0300

    Added the test projects to iter#2-7

---
+++ switch/iter2/tests/InyectorControl/runner/all_tests.c
@@ -0,0 +1,52 @@
+/**
+ *  \file   all_tests.c
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci  francuccilea@gmail.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* ----------------------------- Include files ----------------------------- */
+#include <stdio.h>
+#include "unity_fixture.h"
+
+/* ----------------------------- Local macros ------------------------------ */
+/* ------------------------------- Constants ------------------------------- */
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
+/* ---------------------------- Local variables ---------------------------- */
+/* ----------------------- Local function prototypes ----------------------- */
+/* ---------------------------- Local functions ---------------------------- */
+static void 
+runAllTests(void)
+{
+	RUN_TEST_GROUP(Structure);
+}
+
+/* ---------------------------- Global functions --------------------------- */
+int
+main(int argc, char *argv[])
+{
+    static char *args[8];
+    int nArgs;
+
+    args[0] = argv[0];
+#if 0
+    args[1] = "-g";
+    args[2] = "";
+    nArgs = 3;
+#else
+    nArgs = 1;
+#endif
+
+	UnityMain(nArgs, args, runAllTests);
+    getchar();
+}
+
+/* ------------------------------ End of file ------------------------------ */
---
+++ switch/iter2/tests/InyectorControl/runner/test_InyectorControl_runner.c
@@ -0,0 +1,34 @@
+/**
+ *  \file   test_InyectorControl_runner.c
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci  francuccilea@gmail.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* ----------------------------- Include files ----------------------------- */
+#include "unity_fixture.h"
+
+/* ----------------------------- Local macros ------------------------------ */
+/* ------------------------------- Constants ------------------------------- */
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
+/* ---------------------------- Local variables ---------------------------- */
+/* ----------------------- Local function prototypes ----------------------- */
+/* ---------------------------- Local functions ---------------------------- */
+/* ---------------------------- Global functions --------------------------- */
+TEST_GROUP_RUNNER(Structure)
+{
+	RUN_TEST_CASE(Structure, DefaultStateAfterInit);
+	RUN_TEST_CASE(Structure, AnUnhandledEventDoesNotChangeState);
+	RUN_TEST_CASE(Structure, StateTransitionTableForOff);
+	RUN_TEST_CASE(Structure, StateTransitionTableForStarting);
+}
+
+/* ------------------------------ End of file ------------------------------ */
